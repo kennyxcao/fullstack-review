bc2cdeafd4849fe5c0cf393ef562033965f1c64d
Hack Reactor Student <student@hackreactor.com>
2017-09-22 12:08:33 -0700

    Add basic schema for repoSchema

--- database/index.js
+++ database/index.js
@@ -3,6 +3,17 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  repoID: Number, // id
+  name: String, // name
+  owner: String, // owner.login
+  ownerID: Number, // owner.id
+  description: String, // description
+  htmlURL: String, // html_url
+  cloneURL: String, // clone_url
+  createdAt: Date, // created_at
+  size: Number, // Size
+  forks: Number, // forks
+  watchers: Number, // watchers
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
