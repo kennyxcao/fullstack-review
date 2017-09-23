930f1f73080ac0c27f8268ab36917b8243bf7d16
Hack Reactor Student <student@hackreactor.com>
2017-09-23 11:24:35 -0700

    Install heroku-cli to npm packages list

--- client/src/components/RepoListEntry.jsx
+++ client/src/components/RepoListEntry.jsx
@@ -8,16 +8,15 @@ const RepoListEntry = (props) => (
 
 export default RepoListEntry;
 
-// arr.push({
-//   repoID: repo.id, // id
-//   name: repo.name, // name
-//   owner: repo.owner.login, // owner.login
-//   ownerID: repo.owner.id, // owner.id
-//   description: repo.description, // description
-//   htmlURL: repo['html_url'], // html_url
-//   cloneURL: repo['clone_url'], // clone_url
-//   createdAt: repo['created_at'], // created_at
-//   size: repo.size, // size
-//   forks: repo.forks, // forks
-//   watchers: repo.watchers // watchers  
-// });
+// REPO FORMAT
+// repoID: repo.id, // id
+// name: repo.name, // name
+// owner: repo.owner.login, // owner.login
+// ownerID: repo.owner.id, // owner.id
+// description: repo.description, // description
+// htmlURL: repo['html_url'], // html_url
+// cloneURL: repo['clone_url'], // clone_url
+// createdAt: repo['created_at'], // created_at
+// size: repo.size, // size
+// forks: repo.forks, // forks
+// watchers: repo.watchers // watchers  
